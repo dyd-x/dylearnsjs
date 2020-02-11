7605bd974ad956ae3be8388e11ab0bfd53703eca
dyd-x <38830767+dyd-x@users.noreply.github.com>
2020-02-12 01:12:56 +0800

    12022020 Javascript completed Basics & Syntax!
    yay go dy go

--- Chapter 2/sandbox.js
+++ Chapter 2/sandbox.js
@@ -43,8 +43,18 @@ console.log(fullName.length);
 
 //string methods
 console.log(fullName.toUpperCase());
-let resule = fullName.toLowerCase();
-// console.log(result, fullName);
+let result2 = fullName.toLowerCase();
+console.log(result2, fullName);
+// other string methods!
+let index = result2.indexOf("i");
+let index2 = result2.lastIndexOf("n");
+let result5 = result2.slice(4,10);
+let result6 = result2.substr(4,10);
+// first letter it comes to and replace it.
+let result7 = result2.replace("g","p")
+console.log(result7);
+
+
 
 //Math time!
 
@@ -110,5 +120,122 @@ let ninjas = ['Nicolas', 'Danial', 'Arix'];
 ninjas[1] = 'May';
 console.log(ninjas[1]);
 
-//Will continue more tmr.
+let ages = [20,25,30,35];
+console.log(ages[2]);
 
+// this runs as well
+let random = ['chicken', 'python', 30, 20];
+console.log(random);
+
+//like strings, arrays have properties and methods
+console.log(ninjas.length);
+
+let result8 = ninjas.join(',');
+let result9 = ninjas.join('-');
+
+let result10 =ninjas.indexOf('Nicolas');
+//extend - add more things into the list at the back
+let result11 = ninjas.concat(['ethelda', 'eli']);
+//push on a new value 
+//it alters the original value. its disruptive!
+let result12 = ninjas.push('fuxing');
+//logging the new LENGTH of the new array lmao why length sia so weird
+console.log(result12);
+let results13 = ninjas.pop();
+//it prints the VALUE that was POPPED OFF.
+console.log(results13);
+
+
+//comparison between Null & Undefined
+//setting the variable up but not assigning any value
+let age; 
+//it will be undefined because there is no value!
+console.log(age, age+3, `the age is ${age}`)
+
+//what about null? 
+let age2 = null; 
+//it will be 3!!! the variable title is equal to null there ewill be times where you need this.
+console.log(age2, age2+3, `the age is ${age2}`)
+
+
+//booleans & comparisons
+console.log(true,false, "true", "false");
+
+//methods can return boolean
+let chicken2 = "penguin";
+
+let result14 = chicken2.includes('p');
+let names = ['mario', 'luigi','bob'];
+let result15 =names.includes('luigi');
+
+//comparison operators
+let age3 = 25;
+console.log(age == 25);
+console.log(age == 30);
+console.log(age != 25);
+console.log(age >= 25);
+console.log(age <= 25);
+console.log(age > 25);
+console.log(age < 25);
+
+
+let name = 'ding yang';
+console.log(name == 'ding yang');
+// lowercase letter is bigger than uppercase letters
+console.log(name == 'Ding yang');
+//this will be true! because d is bigger than C
+console.log(name > 'Cing yang');
+
+
+//we saw that we can compare different things and we learnt we can use == to compare
+
+//loose comparison vs strict comparison
+
+//loose comparison (different types can still be equal)
+
+console.log(age3 == 25);
+console.log(age3 == '25');
+console.log( age3 != 25);
+console.log( age3 != '25');
+
+//strict comparison (different types cannot be equal)
+
+console.log(age3 === 25); //true
+console.log(age3 === '25'); // false
+console.log( age3 !== 25); //false
+console.log( age3 !== '25'); //true!
+
+//most of the time it is better to use strict comparisons
+
+//type conversion
+
+let score2 = '100';
+//convert to number
+score = Number(score2);
+console.log(score +1);
+
+//how to check type?
+
+console.log(typeof score2);
+
+let result16 = Number('hello');
+//will return NaN
+console.log(result16);
+
+let result17 = String(5);
+//it will return 5 and it will be type string.
+console.log(result17, typeof result17);
+
+//We can also convert things to boolean
+
+let result18 = Boolean(100);
+//positive values are considered true-y values
+// 0 is considered FALSE.
+// -0 is considered TRUE.
+// negative values are considered false-y values
+console.log(result18, typeof result18);
+
+let result19 = Boolean('0');
+//Strings of any length is true-y
+// but if it is an EMPTY string its false-y
+console.log(result19, typeof result19); 
