319fdad59aff8982466a9e1e6b30f5a677f8f004
dyd-x <38830767+dyd-x@users.noreply.github.com>
2020-02-13 01:20:21 +0800

    13022020 Logic & Consoles!
    long dayyyy did a bit but still consistent!

--- Chapter 3/sandbox.js
+++ Chapter 3/sandbox.js
@@ -47,13 +47,101 @@ if (ninjas.length > 4){
 
 const password = 'p@ssword';
 
-if (password.length >= 8 && password.includes('@')){
+// logical OR || and AND && 
+if (password.length >= 12 && password.includes('@')){
     console.log('that password is long enough');
+} else if (password.length > 8 || password.includes('@') && password.length >= 5) {
+    console.log('this is good enough');
 } else{
-    console.log('this is damn short sia')
+    console.log('this is not good');
 }
 
 
 //logical NOT(!)
+let user = false;
 
-//okay will continue tomorrow
+if (user){
+}
+console.log(true);
+console.log(!true);
+console.log(false);
+console.log(!false);
+
+
+// break and continue
+
+const scores = [50,25,0,30,100,20,10];
+
+for (i = 0; i < scores.length; i++){
+
+    if (scores[i] === 0){
+        // skipped
+        continue;
+    }
+    console.log('your score: ', scores[i]);
+
+    if (scores[i] === 100){
+        console.log('wew you hit max');
+        //get out of the loop yo
+        break;
+    }
+}
+
+const grade = 'D';
+
+//using if statements
+
+if (grade === 'A'){
+
+}else if (grade === 'B'){
+}else if (grade === 'C'){
+}else if (grade === 'D'){
+}else if (grade === 'E'){
+}else if (grade === 'F'){
+}
+    //cause use else if for each and it gets tiring and looks untidy
+// so normally for this we can use switch statements!
+
+//check the value of this variable
+switch(grade){
+    case 'A':
+        console.log('You got an A!');
+    case 'B':
+        console.log('You got an B!');
+    case 'C':
+        console.log('You got an C!');
+    //it will run and D and everything after that
+    //it will run every case after that unless you add a break inside
+
+    case 'D':
+        console.log('You got an D!');
+    case 'E':
+        console.log('You got an E!');
+    default:
+        console.log('not valid');
+
+switch(grade){
+    case 'A':
+        console.log('You got an A!');
+        break;
+    case 'B':
+        console.log('You got an B!');
+        break;
+    case 'C':
+        console.log('You got an C!');
+        break;
+    case 'D':
+        console.log('You got an D!');
+        break;
+    case 'E':
+        console.log('You got an E!');
+        break;
+    default:
+        console.log('not valid');
+        break;
+
+// variables and block scope
+        
+}
+
+
